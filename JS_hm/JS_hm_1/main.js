03fd3d9bc497c2f5ce49e4a53bcd355a140bb379
HaaaamBurger <gemasclashes@gmail.com>
2023-01-26 19:21:11 +0200

    Initial commit

---
+++ JS_hm/JS_hm_1/main.js
@@ -0,0 +1,27 @@
+let w1 = 'hello';
+let w2 = 'owu';
+let w3 = 'com';
+let w4 = 'ua';
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let b1 = true;
+let b2 = false;
+let ent = `${w1} ${w2} ${w3} ${w4} ${n1} ${n2} ${n3} ${n4} ${n5} ${n6} ${n7} ${b1} ${b2}`;
+console.log(ent);
+
+let firstName = 'Sasha';
+let middleName = 'Melnyk';
+let lastName = 'Andriyovych';
+let space = ' ';
+let fullName = firstName + space + middleName + space + lastName;
+console.log(fullName);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a,typeof b,typeof c);
