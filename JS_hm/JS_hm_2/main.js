1999ecebae3b9c703043bbd38245b715f914e049
HaaaamBurger <gemasclashes@gmail.com>
2023-01-28 01:58:33 +0200

    js

---
+++ JS_hm/JS_hm_2/main.js
@@ -0,0 +1,22 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let main = [1,2,3,4,5,6,7,8,9,10];
+console.log(main);
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let f_object = {
+  title : 'FairyTail',
+  pageCount : 356,
+  genre : 'Adventures'
+}
+let s_object = {
+  title : '翻訳しないで',
+  pageCount : 1024,
+  genre : 'Romance'
+}
+let t_object = {
+  title : 'SevenDeadlySins',
+  pageCount : 450,
+  genre : 'Horror'
+}
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
