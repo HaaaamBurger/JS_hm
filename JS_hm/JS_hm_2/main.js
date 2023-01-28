3992ec9e599608001297ca9e97afa2f993aaa29d
HaaaamBurger <gemasclashes@gmail.com>
2023-01-28 19:23:37 +0200

    js

--- JS_hm/JS_hm_2/main.js
+++ JS_hm/JS_hm_2/main.js
@@ -1,5 +1,5 @@
 // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
-let main = [1,2,3,4,5,6,7,8,9,10];
+let main = [1,'word',3,true,5,'number',7,false,9,'bool'];
 console.log(main);
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 let f_object = {
@@ -17,6 +17,37 @@ let t_object = {
   pageCount : 450,
   genre : 'Horror'
 }
-
+  console.log(f_object,s_object,t_object);
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let v_object = {
+  title : 'JujutsuKaisen',
+  pageCount : 982,
+  genre : 'SF',
+  authors : ['name','age']
+}
+let x_object = {
+  title : 'RockPaperScissors',
+  pageCount : 27,
+  genre : 'Games',
+  authors : ['name','age']
+}
+let n_object = {
+  title : 'クソ野郎',
+  pageCount : 2540,
+  genre : 'Drama',
+}
+console.log(v_object,x_object,n_object);
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let user = [
+  {name : 'Sasha',username : 'Hamburger',password : 4589},
+  {name : 'Viktor',username : 'Viktro2003',password : 9821},
+  {name : 'Maks',username : 'Cheburek',password : 6645},
+  {name : 'Stas',username: 'MiKrochelik',password : 3492},
+  {name : 'Vova',username : 'Lowskill23',password : 9123},
+  {name : 'Ebrahim',username : 'Pashtet_102',password : 1923},
+  {name : 'Valentin',username : 'Krosover_%(31)',password: 9043},
+  {name : 'Rostislav',usrename : 'Pelosos_45',password : 2786},
+  {name : 'Erzhan',usrename : 'Vstavay_davay',password : 1094},
+  {name : 'Taras',usrename : '$Vishnya',password : 5812}
+];
+console.log(user[0].password,user[1].password,user[2].password,user[3].password,user[4].password,user[5].password,user[6].password,user[7].password,user[8].password,user[9].password);
