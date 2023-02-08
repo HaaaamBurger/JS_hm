3ebd125f0a3370333dd4ddb89be11f747fae3ae5
HaaaamBurger <gemasclashes@gmail.com>
2023-02-08 14:02:00 +0200

    js

--- JS_hm/JS_hm_4/Additional_2/main.js
+++ JS_hm/JS_hm_4/Additional_2/main.js
@@ -126,22 +126,43 @@
 //             // TO BE CONTINUED .....
 //         ]
 //----------------------------------------------------------------------------------------------------------------------
-// let usersWithCities = [
-//     {
-//         id: 0,
-//         name: '',
-//         age: 0,
-//         status: false,
-//         address: {
-//             user_id: 0,
-//             country: '',
-//             city: ''
+// let usersWithCities = [];
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+// for (let i = 0; i < citiesWithId.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         if (citiesWithId[i].user_id < citiesWithId[j].user_id) {
+//             let temp = citiesWithId[i];
+//             citiesWithId[i] = citiesWithId[j];
+//             citiesWithId[j] = temp;
 //         }
 //     }
-// ];
-//}
-//----------------------------------------------------------------------------------------------------------------------
-
+// }
+// for (let i = 0; i < usersWithId.length; i++) {
+//     let data =  {
+//                 id: usersWithId[i].id,
+//                 name: usersWithId[i].name,
+//                 age: usersWithId[i].age,
+//                 status: usersWithId[i].status,
+//                 address: {
+//                     user_id: citiesWithId[i].user_id,
+//                     country: citiesWithId[i].country,
+//                     city: citiesWithId[i].city
+//                 }
+//             }
+//             usersWithCities.push(data);
+// }
+// console.log(usersWithCities);
 //----------------------------------------------------------------------------------------------------------------------
 // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
 //
