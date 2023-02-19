c3ddd411505e57f2876bab9a9c4fd048f7d4d38a
HaaaamBurger <gemasclashes@gmail.com>
2023-02-20 01:52:33 +0200

    js

--- JS_hm/JS_hm_7(additional)/Additional_part(2)/main.js
+++ JS_hm/JS_hm_7(additional)/Additional_part(2)/main.js
@@ -1,27 +1,28 @@
 function userCard(key,balance = 0,transactionLimit = 0,historyLogs = []) {
+    let mainUser = {key,balance,transactionLimit,historyLogs}
     return {
         getCardOptions(){
             return {
-                balance,
-                transactionLimit,
-                key
+                balance: mainUser.balance,
+                transactionLimit: mainUser.transactionLimit,
+                key: mainUser.key
             }
         },
         putCredits(setBalance){
-            balance += setBalance;
+            mainUser.balance += setBalance;
         },
         takeCredits(getCash){
-            if(getCash <= balance && getCash <= transactionLimit){
-                return balance -= getCash;
+            if(getCash <= mainUser.balance && getCash <= mainUser.transactionLimit){
+                return mainUser.balance -= getCash;
             } else {
                 console.log('Error!');
             }
         },
         setTransactionLimit(setTransactionLimit){
-            transactionLimit = setTransactionLimit;
+            mainUser.transactionLimit = setTransactionLimit;
         },
     }
 }
-let ret = userCard(4,500,100);
+let ret = userCard(4,500,0);
+ret.setTransactionLimit(100);
 ret.takeCredits(0);
-console.log(ret.getCardOptions());
