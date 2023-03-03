0421dd397ac1f64164a37fbb278276b26705c231
HaaaamBurger <gemasclashes@gmail.com>
2023-03-04 00:37:35 +0200

    js

--- JS_hm/MiniProject/post-info.main.js
+++ JS_hm/MiniProject/post-info.main.js
@@ -5,19 +5,19 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
     .then(response => response.json())
     .then(post => {
         let wrapper = document.createElement('div');
-        for (let i = 0; i < post.length; i++) {
-            let postContainer = document.createElement('div');
-            let h2 = document.createElement('h2');
-            h2.innerText = `${post[i].id} - ${post[i].name}`;
-            let body_email = document.createElement('ul');
-            let email = document.createElement('li');
-            email.innerText = post[i].email;
-            let body = document.createElement('li');
-            body.innerText = post[i].email;
-            body_email.append(email,body);
-            postContainer.append(h2,body_email);
-
-            wrapper.append(postContainer);
-        }
+        // for (let i = 0; i < post.length; i++) {
+        //     let postContainer = document.createElement('div');
+        //     let h2 = document.createElement('h2');
+        //     h2.innerText = `${post[i].id} - ${post[i].name}`;
+        //     let body_email = document.createElement('ul');
+        //     let email = document.createElement('li');
+        //     email.innerText = post[i].email;
+        //     let body = document.createElement('li');
+        //     body.innerText = post[i].email;
+        //     body_email.append(email,body);
+        //     postContainer.append(h2,body_email);
+        //
+        //     wrapper.append(postContainer);
+        // }
         document.body.appendChild(wrapper);
     })
--- JS_hm/MiniProject/user-details.main.js
+++ JS_hm/MiniProject/user-details.main.js
@@ -6,22 +6,52 @@ document.body.appendChild(mainContent);
 fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((response) => response.json())
     .then(userData => {
-        // let userBlock = document.createElement('div');
-        // let userInfo = document.createElement('p');
-        // userInfo.innerText = JSON.stringify(userData);
-        //
-        // userBlock.appendChild(userInfo);
-        // mainContent.append(userBlock);
-        for (const prop in userData) {
-            if (typeof userData[prop] === 'object') {
-                for (const innerProp in userData[prop]) {
-                    console.log(`${innerProp}: ${userData[prop][innerProp]}`);
+        // let dataWrapper = document.createElement('div');
+        // for(let user in userData){
+        //     if(typeof userData[user] === 'object'){
+        //         let userObject = userData[user];
+        //         let ul = document.createElement('ul');
+        //         for(let item in userObject){
+        //             let itemObject = userObject[item];
+        //             if(typeof itemObject === 'object'){
+        //                 let itemUl = document.createElement('ul');
+        //                 for(let item in itemObject){
+        //                     let li = document.createElement('li');
+        //                     li.innerText = `${item} - ${itemObject[item]}`;
+        //                     itemUl.appendChild(li);
+        //                 }
+        //                 ul.appendChild(itemUl);
+        //             }else{
+        //                 let li = document.createElement('li');
+        //                 li.innerText = `${item} - ${userObject[item]}`;
+        //                 ul.appendChild(li);
+        //             }
+        //         }
+        //         dataWrapper.appendChild(ul);
+        //     }else{
+        //         let h2 = document.createElement('h2');
+        //         h2.innerText = `${user} - ${userData[user]}`;
+        //         dataWrapper.appendChild(h2);
+        //     }
+        // }
+        function newUserBlock(item, box) {
+            for (const itemKey in item) {
+                if (typeof item[itemKey] !== 'object') {
+                    let p = document.createElement('p');
+                    p.innerText = `${itemKey}:${item[itemKey]}`;
+                    box.appendChild(p);
+                } else {
+                    newUserBlock(item[itemKey], box);
                 }
-            } else {
-                console.log(`${prop}: ${userData[prop]}`);
             }
         }
 
+        let postContainer = document.getElementsByClassName('user')[0];
+        postContainer.style.cssText = 'text-align: center';
+        let personalInfoTitle = document.getElementsByTagName('h2')[0];
+        personalInfoTitle.innerText = 'Personal Info';
+        personalInfoTitle.style.cssText = 'text-align: center';
+        newUserBlock(userData, postContainer)
     });
 
 let buttonContainer = document.createElement('div');
@@ -34,27 +64,31 @@ button.style.cssText = 'width: 90%;height: 50px';
 buttonContainer.appendChild(button);
 
 let postContainer = document.createElement('div');
-postContainer.style.cssText = 'column-count: 5;margin-top: 20px;text-align: center;';
+postContainer.style.cssText = 'display:flex;flex-direction: column;column-count: 5';
 button.onclick = function () {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
         .then((response) => response.json())
         .then((userTitle) => {
-            for (let title of userTitle) {
-                let postWrapper = document.createElement('div');
-                postWrapper.style.cssText = 'padding: 10px;box-sizing: box;align-items: center';
-                let p = document.createElement('p');
-                p.style.margin = '0';
-                p.innerText = title.title;
+            // for (let title of userTitle) {
+            //     let postWrapper = document.createElement('div');
+            //     postWrapper.style.cssText = 'text-align: center';
+            //     let h3 = document.createElement('h3');
+            //     h3.style.margin = '0';
+            //     h3.innerText = title.title;
+            //
+            //     let postButton = document.createElement('button');
+            //     postButton.textContent = 'Post Info';
+            //     postButton.onclick = function () {
+            //         document.location = `post-info.html?userId=${title.id}`;
+            //     }
+            //     postWrapper.append(h3, postButton);
+            //     postContainer.append(postWrapper);
+            //     document.body.appendChild(postContainer);
+            // }
+           for (let objectTitle of userTitle){
+               for(let fieldTitle in objectTitle){
 
-                let postButton = document.createElement('button');
-                postButton.textContent = 'Post Info';
-                postButton.onclick = function () {
-                    document.location = `post-info.html?userId=${title.id}`;
-                }
-                postWrapper.append(p, postButton);
-                postContainer.append(postWrapper);
-                document.body.appendChild(postContainer);
+               }
             }
-
         })
 }
